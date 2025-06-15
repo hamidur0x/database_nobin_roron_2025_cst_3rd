97fd809e2b1beb6c52a5b23a27c684e7ceedb9d8
Md Hamidur Rahman <hamidurrahman0705@gmail.com>
2025-06-15 10:10:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,8 @@ def generate_pdf():
     return send_file(buffer, download_name="students.pdf", as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render sets PORT env variable
+    app.run(host='0.0.0.0', port=port)
 
 
 
